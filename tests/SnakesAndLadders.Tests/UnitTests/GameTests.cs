42f5e8f0f1c6fdb5a4439c7bcced113d96cc13a8
Manel Pérez <manel.parez@redarbor.csccsl.com>
2022-09-18 08:24:49 +0200

    initial commit

---
+++ tests/SnakesAndLadders.Tests/UnitTests/GameTests.cs
@@ -0,0 +1,21 @@
+namespace SnakesAndLadders.Tests.UnitTests
+{
+    public class GameTests
+    {
+        //### US 1 - Token Can Move Across the Board
+        //As a player
+        //I want to be able to move my token
+        //So that I can get closer to the goal    
+
+        //**UAT1**
+        //Given the game is started
+        //When the token is placed on the board
+        //Then the token is on square 1
+        [Theory]
+        [InlineData(1)]
+        public void US_1_UAT1(int expected)
+        {
+            
+        }
+    }
+}
